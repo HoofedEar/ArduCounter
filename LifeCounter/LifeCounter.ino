798f586e1b80b2cbea639b06d66d4b6a6ae8360e
Chris Vergilio <HoofedEar@users.noreply.github.com>
2019-03-29 16:51:24 -0700

    Major update
    -Added 3 different types of life trackers (2 player, Commander, and Simple)

--- LifeCounter/LifeCounter.ino
+++ LifeCounter/LifeCounter.ino
@@ -13,6 +13,13 @@ const unsigned char experience[] PROGMEM {
   0x3, 0x3f, 0x7f, 0xc4, 0xc4, 0x7f, 0x3f, 0x3, 
 };
 
+const unsigned char blue[] PROGMEM {
+  0x60, 0xf8, 0xfc, 0x3e, 0xe, 0x7, 0x3, 0x9e, 0x3e, 0xfc, 0xf8, 0x60, 0x00, 0x1, 0x3, 0x6, 0x4, 0x8, 0x8, 0x4, 0x6, 0x3, 0x3, 0x00,  
+};
+
+// Mode for type of counter (0 = Select, 1 = 2 players, 2 = Commander, 3 = Simple)
+int gamemode = 0;
+
 // Life totals
 int player1 = 20;
 int player2 = 20;
@@ -37,12 +44,31 @@ int expP2 = 0;
 void setup() {
   arduboy.begin();
   arduboy.setFrameRate(30);
-
-  // initialize things here
-
 }
 
-void counterControl() {
+
+// Select A for Standard, B for Commander.
+void gameSelect() {
+  arduboy.print("   MTG LIFE COUNTER\n   ----------------\nSelect a mode.\nL  - 2 Players (20)\nR  - Commander (40)\nUP - Simple (20)");
+  arduboy.drawBitmap(3, 50, blue, 12, 12, WHITE);
+  arduboy.setCursor(21, 52);
+  arduboy.print("made by chris v.");
+  if (arduboy.justPressed(LEFT_BUTTON)) {
+      gamemode = 1;
+  }
+    else if (arduboy.justPressed(RIGHT_BUTTON)) {
+      player1 = 35;
+      gamemode = 2;
+  }
+  else if (arduboy.justPressed(UP_BUTTON)) {
+      player1 = 19;
+      gamemode = 3;
+  }
+}
+/*
+ * THIS BLOCK BEGINS THE STANDARD SETTINGS
+ */
+void standardControl() {
   
   //Controls for the life counter
   //Left is always Player 1
@@ -130,7 +156,7 @@ void counterControl() {
   
 }
 
-void drawCounter()
+void drawStandard()
 {
   // We have the make our own "padding" for the 0's
   arduboy.setTextSize(5);
@@ -196,6 +222,142 @@ void drawCounter()
     arduboy.print(expP2);
   }
 }
+/*
+ * THIS BLOCK BEGINS THE COMMANDER SETTINGS
+ */
+void commanderControl() {
+  
+  //Controls for the life counter
+  //Left is always Player 1
+  //Right is always Player 2
+  if (arduboy.justPressed(UP_BUTTON) && player1 < 999) {
+    player1 += 1;
+  }
+  if (arduboy.justPressed(DOWN_BUTTON) && player1 > 0) {
+    player1 -= 1;
+  }
+  if (arduboy.justPressed(RIGHT_BUTTON) && player1 < 999) {
+    player1 += 5;
+  }
+  if (arduboy.justPressed(LEFT_BUTTON) && player1 > 0) {
+    player1 -= 5;
+  }
+  
+  // Reset life totals and counters
+  // Some values are set differently for QOL
+  if (arduboy.justPressed(A_BUTTON) && arduboy.justPressed(B_BUTTON) && 
+      arduboy.justPressed(UP_BUTTON)) {
+    player1 = 40;
+    
+    poisonP1 = 0;
+    expP1 = -1;
+    poisonP1 = -1;
+    
+    counterP1 = 1;
+  }
+  
+  // Increment the counters
+  if (arduboy.justPressed(A_BUTTON)) {
+      if (poisonP1 < 10) {
+        poisonP1 += 1;
+      }
+  }
+  
+  // Change the counter type
+  if (arduboy.justPressed(B_BUTTON)) {
+    if (expP1 < 99) {
+        expP1 += 1;
+      }
+  }
+  
+}
+
+void drawCommander()
+{
+  // We have the make our own "padding" for the 0's
+  arduboy.print("Player 1");
+  arduboy.setTextSize(5);
+  arduboy.setCursor(0, 25);
+  if (player1 < 10) {
+    
+    arduboy.print(0);
+    arduboy.print(player1);
+  }
+  else {
+    arduboy.print(player1);
+  }
+  
+  arduboy.setTextSize(2);
+  arduboy.print(" ");
+
+  /* Draw a divider between life and counters
+  arduboy.drawFastVLine((WIDTH / 2), 1, HEIGHT, WHITE);
+  arduboy.drawFastVLine((WIDTH / 2) - 1, 1, HEIGHT, WHITE);
+  arduboy.drawFastVLine((WIDTH / 2) - 2, 1, HEIGHT, WHITE);
+  */
+  
+  // Draw our types of counters
+  arduboy.drawBitmap(91, 28, poison, 8, 8, WHITE);
+  arduboy.setCursor(103, 25);
+  arduboy.setTextSize(2);
+  arduboy.print(poisonP1);
+  
+  arduboy.drawBitmap(91, 50, experience, 8, 8, WHITE);
+  arduboy.setCursor(103, 47);
+  arduboy.setTextSize(2);
+  arduboy.print(expP1);
+
+}
+/*
+ * THIS BLOCK END THE COMMANDER SETTINGS
+ */
+ 
+/*
+ * SIMPLE CONTROLS
+ */
+ void simpleControl() {
+  
+  //Controls for the life counter
+  if (arduboy.justPressed(UP_BUTTON) && player1 < 999) {
+    player1 += 1;
+  }
+  if (arduboy.justPressed(DOWN_BUTTON) && player1 > 0) {
+    player1 -= 1;
+  }
+  if (arduboy.justPressed(RIGHT_BUTTON) && player1 < 995) {
+    player1 += 5;
+  }
+  if (arduboy.justPressed(LEFT_BUTTON) && player1 > 5) {
+    player1 -= 5;
+  }
+  
+  // Reset life totals and counters
+  // Some values are set differently for QOL
+  if (arduboy.justPressed(A_BUTTON) && arduboy.justPressed(B_BUTTON)) {
+    player1 = 20;
+  }
+  
+}
+
+void drawSimple()
+{
+  // We have the make our own "padding" for the 0's
+  arduboy.setTextSize(2);
+  arduboy.print("Player 1");
+  arduboy.setTextSize(5);
+  arduboy.setCursor((WIDTH / 2) - 25, 25);
+  if (player1 < 10) {
+    
+    arduboy.print(0);
+    arduboy.print(player1);
+  }
+  else {
+    arduboy.print(player1);
+  }
+
+}
+ 
+ 
 
 void loop() {
   
@@ -204,10 +366,24 @@ void loop() {
   arduboy.clear();
   arduboy.pollButtons();
 
-  counterControl();
-  
-  // Draw the life counter
-  drawCounter();
+  // Choose the game mode first
+  if (gamemode == 0) {
+    gameSelect();
+  }
+
+  // Draw the standard counter
+  if (gamemode == 1) {
+    standardControl();
+    drawStandard();
+  }
+  if (gamemode == 2) {
+    commanderControl();
+    drawCommander();
+  }
+  if (gamemode == 3) {
+    simpleControl();
+    drawSimple();
+  }
 
   arduboy.display();
 }
